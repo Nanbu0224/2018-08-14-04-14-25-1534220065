9c9790b7a5a159e99267124d4cbc99fc33f94d5a
Nanbu0224 <40815063+Nanbu0224@users.noreply.github.com>
2019-05-31 10:50:57 +0800

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(theInputNum) {
+	if (theInputNum=='910')
+	{
+		return "._. ... ._.\n"+"|_| ..| |.|\n"+"..| ..| |_|\n";
+	}
+	if (theInputNum=='256')
+	{
+		return "._. ._. ._.\n"+"._| |_. |_.\n"+"|_. ._| |_|\n";
+	}	
+	if (theInputNum=='7')
+	{
+		return "._.\n"+"..|\n"+"..|\n";
+	}
 };
